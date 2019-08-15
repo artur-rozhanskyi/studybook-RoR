ada6c49e2615da0cc148484fdff93ae3ab976caf
Artur Rozhanskyi <artur.rozhanskyi@nixsolutions.com>
2019-08-15 18:03:59 +0300

    Carriage type counter

--- app/helpers/trains_helper.rb
+++ app/helpers/trains_helper.rb
@@ -1,2 +1,8 @@
 module TrainsHelper
+  def count_carraiage_types(train)
+    train.carriages.each_with_object({}) do |carriage, hsh|
+      hsh[carriage.carriage_type.name] ||= 0
+      hsh[carriage.carriage_type.name] += 1
+    end
+  end
 end
