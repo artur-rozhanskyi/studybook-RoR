6e009a7145f484f20dc6cabe7b91037da710ce7e
Artur Rozhanskyi <artur.rozhanskyi@nixsolutions.com>
2019-08-16 22:25:57 +0300

    Amount places in carriages 2

--- app/helpers/trains_helper.rb
+++ app/helpers/trains_helper.rb
@@ -1,14 +1,2 @@
 module TrainsHelper
-  def count_carraiage_types(train)
-    train.carriages.each_with_object(Hash.new(0)) do |carriage, hsh|
-      hsh[carriage.carriage_type.name] += 1
-    end
-  end
-
-  def count_carraiage_places(train)
-    train.carriages.each_with_object(upper_places: Hash.new(0), bottom_places: Hash.new(0)) do |carriage, hsh|
-      hsh[:upper_places][carriage.carriage_type.name] += carriage.upper_places
-      hsh[:bottom_places][carriage.carriage_type.name] += carriage.bottom_places
-    end
-  end
 end
--- app/models/train.rb
+++ app/models/train.rb
@@ -6,4 +6,31 @@ class Train < ApplicationRecord
   has_and_belongs_to_many :routes
 
   has_many :carriages, dependent: :nullify
+
+  def count_carraiage_types
+    carriages.each_with_object(Hash.new(0)) do |carriage, hsh|
+      hsh[carriage.carriage_type.name] += 1
+    end
+  end
+
+  def count_carraiage_places
+    # 1 version - Abc Metric - 19
+    # types = CarriageType.all.find_each.with_object({}) { |obj, hsh| hsh[obj.id] = obj.name }
+    # hsh = Hash.new { |hash, key| hash[key] = Hash.new(0) }
+    # carriages.each do |carriage|
+    #   hsh[types[carriage.carriage_type_id]][:upper_places] += carriage.upper_places
+    #   hsh[types[carriage.carriage_type_id]][:bottom_places] += carriage.bottom_places
+    # end
+    # hsh
+
+    # 2 version
+    # result = ActiveRecord::Base.connection.execute("SELECT carriages.*, carriage_types.name FROM carriages
+    #   INNER JOIN carriage_types ON carriage_types.id = carriages.carriage_type_id")
+    # hsh = Hash.new { |hash, key| hash[key] = Hash.new(0) }
+    # result.each do |element|
+    #   hsh[element['name']][:bottom_places] += element['bottom_places']
+    #   hsh[element['name']][:upper_places] += element['upper_places']
+    # end
+    # hsh
+  end
 end
